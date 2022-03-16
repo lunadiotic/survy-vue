22285ae64979d32ab5c19b9f5ba3be9647fb6032
aimeliala <ahmadhakim404@gmail.com>
2022-03-16 15:18:45 +0700

    add vue-router and sample component

--- src/App.vue
+++ src/App.vue
@@ -20,18 +20,8 @@ const handleSubmit = () => {
 </script>
 
 <template>
-	<img alt="Vue logo" src="./assets/logo.png" />
-  <button @click="handleSubmit">Authentication</button>
-  {{ store.user }}
+  <router-view></router-view>
 </template>
 
 <style>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import axios from 'axios'
 
 import './app.css'
+import router from './router'
 
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'http://localhost:8000/'
@@ -11,4 +12,6 @@ axios.defaults.baseURL = 'http://localhost:8000/'
 const store = createPinia()
 const app = createApp(App)
 
-app.use(store).mount('#app')
+app.use(router)
+    .use(store)
+    .mount('#app')
