71e6c1b9606dca53f9dcd22569e8c8e882f77785
aimeliala <ahmadhakim404@gmail.com>
2022-03-22 12:58:42 +0700

    layouting in vue with vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import Main from '../components/Main.vue'
 
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
 	{
 		path: '/',
-		name: 'Dashboard',
+		redirect: '/dashboard',
 		component: Dashboard,
+		children: [
+			{ path: '/dashboard', 'name': 'Dashboard', component: Main }
+		]
 	},
 	{
 		path: '/login',
