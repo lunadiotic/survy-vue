ee6235d2d6c77210ed6424f9b2b6163e7945ac4d
lionatic <ahmadhakim404@gmail.com>
2022-03-28 22:32:11 +0700

    create survey card with data from pinia

--- src/components/Survey.vue
+++ src/components/Survey.vue
@@ -1,20 +1,97 @@
 <template>
 	<header class="bg-white shadow">
 		<div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-			<h1 class="text-3xl font-bold text-gray-900">Survey</h1>
+			<div class="flex justify-between items-center">
+				<h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
+				<router-link
+					to="#"
+					class="py-2 px-3 text-white bg-emerald-500 rounded-md hover:bg-emerald-600"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-4 w-4 -mt-1 inline-block"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						stroke-width="2"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M12 4v16m8-8H4"
+						/>
+					</svg>
+					Add Survey
+				</router-link>
+			</div>
 		</div>
 	</header>
 	<main>
 		<div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-			<!-- Replace with your content -->
-			<div class="px-4 py-6 sm:px-0">
-				<div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+			<div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
+				<div
+					v-for="survey in surveyData"
+					:key="survey.id"
+					class="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-50 h-[470px] rounded-lg"
+				>
+					<img
+						:src="survey.image"
+						alt="{{ survey.title }}"
+						class="w-full h-48 object-cover"
+					/>
+					<h4 class="mt-4 text-lg font-bold">{{ survey.title }}</h4>
+					<div v-html="survey.description" class="overflow-hidden flex-1"></div>
+
+					<div class="flex justify-between items-center mt-3">
+						<router-link
+							to="#"
+							class="flex py-2 px-4 border border-transparent text-sm rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-5 w-5 mr-2"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								stroke-width="2"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+								/>
+							</svg>
+							Edit
+						</router-link>
+						<button
+							class="h-8 w-8 flex items-center justify-center rounded-full border border-transparent text-sm text-red-500 focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-5 w-5 -mt-1 inline-block"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								stroke-width="3"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+								/>
+							</svg>
+						</button>
+					</div>
+				</div>
 			</div>
-			<!-- /End replace -->
 		</div>
 	</main>
 </template>
 
-<script setup></script>
+<script setup>
+import { useSurvey } from '../store/survey';
+const survey = useSurvey();
+const surveyData = survey.surveys;
+</script>
 
 <style scoped></style>
---
+++ src/store/survey.js
@@ -0,0 +1,45 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+import { ref } from 'vue';
+
+export const useSurvey = defineStore('survey-store', () => {
+	const surveys = ref([
+		{
+			id: 100,
+			title: 'The survey from idstack',
+			slug: 'the-survey-from-idstack',
+			status: 'draft',
+			image:
+				'https://img.freepik.com/free-vector/futuristic-technological-wallpaper_79603-1093.jpg',
+			description:
+				'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam eius fugiat, ducimus necessitatibus harum illum quisquam possimus velit itaque placeat tenetur modi cumque enim provident nihil in odio laboriosam quasi!',
+			expired_at: '2022-03-30 20:00:00',
+			created_at: '2022-03-30 20:00:00',
+			updated_at: '2022-03-30 20:00:00',
+			questions: [
+				{
+					id: 1,
+					type: 'select',
+					question: "what's the color of rose?",
+					description: null,
+					data: {
+						options: [
+							{
+								uuid: '123asdf-123sfal-123asd-123asd-321333',
+								text: 'blue',
+							},
+							{
+								uuid: '123asdf-123sfal-123asd-123asd-123121',
+								text: 'blue',
+							},
+						],
+					},
+				},
+			],
+		},
+	]);
+
+	return {
+		surveys,
+	};
+});
